b3011bc6717ad5802d306d37d2c5908777a09176
Fidalgu <131529761+Fidalgu@users.noreply.github.com>
2023-06-08 04:23:36 +0100

    ROTINA2505.py

--- ROTINA2505.py
+++ ROTINA2505.py
@@ -243,49 +243,7 @@ while True:
         #control.order[0] = cmd.CMD_STOP
         #control.run()
         #control.stop()
-'''        
-import math
-from Control import *
-from Servo import *
-class Fidalction:
-    def __init__(self):
-        self.servo=Servo()
-        self.control=Control()
-        self.servo.setServoAngle(15,90)
-    def jump_dalgo(self):
-        xyz=[[0,50,0],[-100,23,0],[-100,23,0],[0,50,0]]
-        for i in range(4):
-            xyz[i][0]=(xyz[i][0]-self.control.point[i][0])/30
-            xyz[i][1]=(xyz[i][1]-self.control.point[i][1])/30
-            xyz[i][2]=(xyz[i][2]-self.control.point[i][2])/30
-        for j in range(30):
-            for i in range(4):
-                self.control.point[i][0]+=xyz[i][0]
-                self.control.point[i][1]+=xyz[i][1]
-                self.control.point[i][2]+=xyz[i][2]
-            self.control.run()
-            time.sleep(0.01)
-        for i in range(4):
-            for i in range(50,120,1):
-                self.control.point[0][1]=i
-                self.control.point[3][1]=i
-                self.control.run()
-                time.sleep(0.01)
-            for i in range(120,50,-1):
-                self.control.point[0][1]=i
-                self.control.point[3][1]=i
-                self.control.run()
-                time.sleep(0.01)
-        xyz=[[55,78,0],[55,78,0],[55,78,0],[55,78,0]]
-        for i in range(4):
-            xyz[i][0]=(xyz[i][0]-self.control.point[i][0])/30
-            xyz[i][1]=(xyz[i][1]-self.control.point[i][1])/30
-            xyz[i][2]=(xyz[i][2]-self.control.point[i][2])/30
-        for j in range(30):
-            for i in range(4):
-                self.control.point[i][0]+=xyz[i][0]
-                self.control.point[i][1]+=xyz[i][1]
-                self.control.point[i][2]+=xyz[i][2]
+
             self.control.run()
             time.sleep(0.01)
 '''
